19d8ada55423ea40c6f57fb1ac4e21910237c04c
Coleman <coleman.donaldson@gmail.com>
2020-04-16 20:12:31 +0200

    Update footnotes.py

--- ocr_3-page-headers-gone/footnotes.py
+++ ocr_3-page-headers-gone/footnotes.py
@@ -18,23 +18,37 @@ lineNumber = 0
 with open(filename, 'r') as inputFile:
     print("Working on file: " + filename)
     
-    # Make a list of everything
-    inputList = inputFile.read().split("n")
+    pageNumber = 147
     
     # Loop through it and make a list of only the page-paragraphs
-    for line in inputList:
-        
+    for line in inputFile:
         if line.startswith("###"):
             continue
-        elif line.starts("\n"):
+        elif line == "":
             continue
         else:
-            pageList += line
+            pageList.append(line)
     
+    # Loop through the list of page-paragraphs 
     for page in pageList:
+        # Split it on the likely footnote marker
+        pageParts = re.split("\s\d\.\s", page)
         
-        if         footnoteMatch = re.match(r"\s\d\.\s", line)
-
+        # Output the page's text proper
+        output += pageParts[0] + "\n\n"
+        
+        # Split off the footnote section of the page
+        footNoteSection = pageParts[1:]
+        
+        footnoteNum = 1
+        
+        # Loop through the footnote section and output it in the new format
+        for footnote in footNoteSection:
+            output += "[^MD" + str(pageNumber) + "-" + str(footnoteNum) + "]: " + footnote + "\n\n"
+            footnoteNum += 1
+        
+        pageNumber += 1
+                
 # Open an output file and write to it
     with open("output.txt", "w") as fileOut:
          fileOut.write(output)
